9db9556da744818252fefdee5e39eaec13d88c0f
msanlisavas <muratsanlisavas@gmail.com>
2026-04-21 16:02:05 +0300

    test(ws): scaffold CoinGecko.Api.WebSockets.Tests project

---
+++ tests/CoinGecko.Api.WebSockets.Tests/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Net.WebSockets;
+global using System.Threading;
+global using System.Threading.Tasks;
+global using Shouldly;
+global using Xunit;
