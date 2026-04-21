35d7b9e3521487c7d82c00a4c50c8507f8f8af5b
msanlisavas <muratsanlisavas@gmail.com>
2026-04-21 14:29:50 +0300

    feat(api): add CoinGeckoClient root implementation

---
+++ src/CoinGecko.Api/CoinGeckoClient.cs
@@ -0,0 +1,35 @@
+using CoinGecko.Api.Resources;
+
+namespace CoinGecko.Api;
+
+internal sealed class CoinGeckoClient(
+    ICoinsClient coins,
+    INftsClient nfts,
+    IExchangesClient exchanges,
+    IDerivativesClient derivatives,
+    ICategoriesClient categories,
+    IAssetPlatformsClient assetPlatforms,
+    ICompaniesClient companies,
+    ISimpleClient simple,
+    IGlobalClient global,
+    ISearchClient search,
+    ITrendingClient trending,
+    IOnchainClient onchain,
+    IKeyClient key,
+    IPingClient ping) : ICoinGeckoClient
+{
+    public ICoinsClient          Coins          { get; } = coins;
+    public INftsClient           Nfts           { get; } = nfts;
+    public IExchangesClient      Exchanges      { get; } = exchanges;
+    public IDerivativesClient    Derivatives    { get; } = derivatives;
+    public ICategoriesClient     Categories     { get; } = categories;
+    public IAssetPlatformsClient AssetPlatforms { get; } = assetPlatforms;
+    public ICompaniesClient      Companies      { get; } = companies;
+    public ISimpleClient         Simple         { get; } = simple;
+    public IGlobalClient         Global         { get; } = global;
+    public ISearchClient         Search         { get; } = search;
+    public ITrendingClient       Trending       { get; } = trending;
+    public IOnchainClient        Onchain        { get; } = onchain;
+    public IKeyClient            Key            { get; } = key;
+    public IPingClient           Ping           { get; } = ping;
+}
